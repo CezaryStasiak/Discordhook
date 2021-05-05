b703c3eb49aa37f13a7f6a5707c02038c2b8be6c
CezaryStasiak <cezary.p.stasiak@gmail.com>
2021-05-05 20:39:20 +0200

    init commit

--- src/test/java/cezarystasiak/discordhook/DiscordhookTest.java
+++ src/test/java/cezarystasiak/discordhook/DiscordhookTest.java
@@ -2,17 +2,10 @@ package cezarystasiak.discordhook;
 
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 public class DiscordhookTest {
 
     @Test
-    public void should_create_object() throws IOException {
-        String url = "https://discord.com/api/webhooks/742736718960001067" +
-                "/A1SUc1fZv1CnCTWr6bkH8pBuPjFHa93SBPIky6RIspg7ZXQyMjVviOlzjTTdEEA8EBeN";
-
-        new Discordhook(url).toss("tralala");
+    public void should_create_object() {
     }
 
 }
